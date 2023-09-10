9cc6121fd7c3ac82b56bda3b1b3340ed8bf4fd5d
Mabala-Karabo <mabalakarabo25@gmail.com>
2023-09-10 23:01:07 +0200

    Add project files.

---
+++ TryAndCatch4/Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        txtNum = New TextBox()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(588, 291)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(112, 34)
+        Button1.TabIndex = 0
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(266, 22)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(213, 25)
+        Label1.TabIndex = 1
+        Label1.Text = "Structured Error Handling"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(284, 77)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(160, 25)
+        Label2.TabIndex = 2
+        Label2.Text = "(System Exception)"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(79, 175)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(119, 25)
+        Label3.TabIndex = 3
+        Label3.Text = "Enter number"
+        ' 
+        ' txtNum
+        ' 
+        txtNum.Location = New Point(202, 186)
+        txtNum.Name = "txtNum"
+        txtNum.Size = New Size(150, 31)
+        txtNum.TabIndex = 4
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(txtNum)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(Button1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtNum As TextBox
+End Class
---
+++ TryAndCatch4/Form1.vb
@@ -0,0 +1,18 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim number1 As String
+        number1 = txtNum.Text
+        Try
+            'taking the value from the user and convert it to integer data type if it fails then catchbox follows
+            number1 = CInt(txtNum.Text)
+            'displaying the number the user entered if the an ineteger is entered
+            MsgBox("You have entered " & number1)
+        Catch ex As Exception
+            'if try box fails then catch box will follow printing out the error on the screen
+            MsgBox("Error, you have entered a non-numeric value")
+            MsgBox(ex.ToString)
+        Finally
+            MsgBox("We are done here")
+        End Try
+    End Sub
+End Class
